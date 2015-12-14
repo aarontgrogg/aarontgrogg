2eb830e6bb5fc7761a032748edb54f30c00636a9
Aaron T. Grogg <aarontgrogg@gmail.com>
2015-12-14 13:24:15 +0100

    removing that damned emoji crap

--- wp-content/themes/boilerplate/functions.php
+++ wp-content/themes/boilerplate/functions.php
@@ -625,6 +625,36 @@ endif;
 	}
 	add_filter( 'comment_form_field_comment', 'boilerplate_comment_field_placeholder' );
 
+/**
+ * Remove the damned emjoi crap that was added to WP Core as of 4.2...
+ * from: http://wordpress.stackexchange.com/questions/185577/disable-emojicons-introduced-with-wp-4-2
+ */
+
+	function boilerplate_disable_wp_emojicons() {
+		// all actions related to emojis
+		remove_action( 'admin_print_styles', 'print_emoji_styles' );
+		remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+		remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+		remove_action( 'wp_print_styles', 'print_emoji_styles' );
+		remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+		remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+		remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
+		// filter to remove TinyMCE emojis
+		add_filter( 'tiny_mce_plugins', 'boilerplate_disable_emojicons_tinymce' );
+	}
+	/*
+	 * Adn just in case you use tiny_mce...
+	 */
+	function boilerplate_disable_emojicons_tinymce( $plugins ) {
+		if ( is_array( $plugins ) ) {
+			return array_diff( $plugins, array( 'wpemoji' ) );
+		} else {
+			return array();
+		}
+	}
+	add_action( 'init', 'boilerplate_disable_wp_emojicons' );
+
+
 /*	End Boilerplate */
 
 ?>
