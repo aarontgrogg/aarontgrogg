e4f7b0be982399f2e134fbab309751ee3eac1989
Aaron T. Grogg <aarontgrogg@gmail.com>
2015-12-23 11:54:24 +0100

    removing cruft

--- dist/lab/node/http.js
+++
@@ -1,8 +0,0 @@
-var http = require("http");
-
-http.createServer(function(request, response) {
-	request.on("end", function() {
-		response.writeHead(200, {"Content-Type" : "text/plain"});
-		response.end("Hello HTTP!");
-	});
-}).listen(8080);
--- dist/lab/service-worker/trained-to-thrill/js/index.js
+++
@@ -1,6 +0,0 @@
-// register the service worker
-navigator.serviceWorker.register('service-worker.js').then(function(registration) {
-  console.log('[register] registered: ', registration);
-}).catch(function(error){
-  console.log('[register] NOT registered: ', error);
-});
