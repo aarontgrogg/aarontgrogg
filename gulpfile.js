c856e01b8d2eb0b370018a90e30d347f3738c657
Aaron T. Grogg <aarontgrogg@gmail.com>
2015-12-23 13:41:13 +0100

    testing deploybot build

--- gulpfile.js
+++ gulpfile.js
@@ -20,7 +20,6 @@ var gulp = require('gulp'),
 // run theme CSS-related tasks
 gulp.task( 'styles-theme', function() {
     return gulp.src( 'src/styles/*.css' )                   // grab all src css files
-        //.pipe( plugins.changedInPlace() )                   // check if source has changed since last build
         .pipe( plugins.autoprefixer( AUTOPREFIXER_MATRIX ) ) // remove unneeded and add needed prefixes
         .pipe( plugins.concat( 'style-min.css' ) )          // concatenate all css files into a single files
         .pipe( plugins.minifyCss() )                        // minify concatenated css files
@@ -30,7 +29,6 @@ gulp.task( 'styles-theme', function() {
 // run theme JS-related tasks
 gulp.task( 'scripts-theme', function() {
     return gulp.src( 'src/scripts/*.js' )                   // grab all src js files
-        //.pipe( plugins.changedInPlace() )                   // check if source has changed since last build
         .pipe( plugins.concat( 'scripts-min.js' ) )         // concatenate all js files into a single files
         .pipe( plugins.uglify() )                           // minify concatenated js files
         .pipe( gulp.dest( THEME_DIST_DIR ) );               // save files into dist directory
@@ -39,7 +37,6 @@ gulp.task( 'scripts-theme', function() {
 // run plugins CSS-related tasks
 gulp.task( 'styles-plugins', function() {
     return gulp.src( PLUGIN_DIST_DIR + '**/*.css' )         // grab all dist css files
-        //.pipe( plugins.changedInPlace() )                   // check if source has changed since last build
         .pipe( plugins.autoprefixer( AUTOPREFIXER_MATRIX ) ) // remove unneeded and add needed prefixes
         .pipe( gulp.dest( PLUGIN_DIST_DIR) )                // save files into dist directory
         .pipe( plugins.minifyCss() )                        // minify concatenated css files
@@ -49,7 +46,6 @@ gulp.task( 'styles-plugins', function() {
 // run plugins JS-related tasks
 gulp.task( 'scripts-plugins', function() {
     return gulp.src( PLUGIN_DIST_DIR + '**/*.js' )          // grab all src js files
-        //.pipe( plugins.changedInPlace() )                   // check if source has changed since last build
         .pipe( plugins.uglify() )                           // minify concatenated js files
         .pipe( gulp.dest( PLUGIN_DIST_DIR ) );               // save files into dist directory
 });
@@ -57,15 +53,14 @@ gulp.task( 'scripts-plugins', function() {
 // run image-related tasks
 gulp.task( 'images', function() {
     return gulp.src( UPLOADS_DIST_DIR + '**/**' )           // grab all dist image files
-        //.pipe( plugins.changedInPlace() )                   // check if source has changed since last build
-        .pipe( plugins.webp() )                             // create WEBP versions of images
-        .pipe( plugins.imagemin(                            // optimize & cache images in the uploads directory
+        .pipe( plugins.cache( plugins.webp() ) )            // create WEBP versions of images
+        .pipe( plugins.cache( plugins.imagemin(             // optimize & cache images in the uploads directory
             {
                 optimizationLevel: 3, 
                 progressive: true, 
                 interlaced: true
             }
-        ))
+        ) ) )
         .pipe( gulp.dest( UPLOADS_DIST_DIR ) );               // save files into dist directory
 });
 
