5fa72cfd62376d310d3d2ae39c3077ecf336f08d
Aaron T. Grogg <aarontgrogg@gmail.com>
2015-12-23 13:35:18 +0100

    testing deploybot build

--- gulpfile.js
+++ gulpfile.js
@@ -20,48 +20,44 @@ var gulp = require('gulp'),
 // run theme CSS-related tasks
 gulp.task( 'styles-theme', function() {
     return gulp.src( 'src/styles/*.css' )                   // grab all src css files
-        .pipe( plugins.changedInPlace() )                   // check if source has changed since last build
+        //.pipe( plugins.changedInPlace() )                   // check if source has changed since last build
         .pipe( plugins.autoprefixer( AUTOPREFIXER_MATRIX ) ) // remove unneeded and add needed prefixes
         .pipe( plugins.concat( 'style-min.css' ) )          // concatenate all css files into a single files
         .pipe( plugins.minifyCss() )                        // minify concatenated css files
-        .pipe( gulp.dest( THEME_DIST_DIR ) )                // save files into dist directory
-        .pipe( plugins.notify( 'Completed styles-theme' ) ); // send completion notification
+        .pipe( gulp.dest( THEME_DIST_DIR ) );               // save files into dist directory
 });
 
 // run theme JS-related tasks
 gulp.task( 'scripts-theme', function() {
     return gulp.src( 'src/scripts/*.js' )                   // grab all src js files
-        .pipe( plugins.changedInPlace() )                   // check if source has changed since last build
+        //.pipe( plugins.changedInPlace() )                   // check if source has changed since last build
         .pipe( plugins.concat( 'scripts-min.js' ) )         // concatenate all js files into a single files
         .pipe( plugins.uglify() )                           // minify concatenated js files
-        .pipe( gulp.dest( THEME_DIST_DIR ) )                // save files into dist directory
-        .pipe( plugins.notify( 'Completed scripts-theme' ) ); // send completion notification
+        .pipe( gulp.dest( THEME_DIST_DIR ) );               // save files into dist directory
 });
 
 // run plugins CSS-related tasks
 gulp.task( 'styles-plugins', function() {
     return gulp.src( PLUGIN_DIST_DIR + '**/*.css' )         // grab all dist css files
-        .pipe( plugins.changedInPlace() )                   // check if source has changed since last build
+        //.pipe( plugins.changedInPlace() )                   // check if source has changed since last build
         .pipe( plugins.autoprefixer( AUTOPREFIXER_MATRIX ) ) // remove unneeded and add needed prefixes
         .pipe( gulp.dest( PLUGIN_DIST_DIR) )                // save files into dist directory
         .pipe( plugins.minifyCss() )                        // minify concatenated css files
-        .pipe( gulp.dest( PLUGIN_DIST_DIR) )                // save files into dist directory
-        .pipe( plugins.notify( 'Completed styles-plugins' ) ); // send completion notification
+        .pipe( gulp.dest( PLUGIN_DIST_DIR) );               // save files into dist directory
 });
 
 // run plugins JS-related tasks
 gulp.task( 'scripts-plugins', function() {
     return gulp.src( PLUGIN_DIST_DIR + '**/*.js' )          // grab all src js files
-        .pipe( plugins.changedInPlace() )                   // check if source has changed since last build
+        //.pipe( plugins.changedInPlace() )                   // check if source has changed since last build
         .pipe( plugins.uglify() )                           // minify concatenated js files
-        .pipe( gulp.dest( PLUGIN_DIST_DIR ) )               // save files into dist directory
-        .pipe( plugins.notify( 'Completed scripts-plugins' ) ); // send completion notification
+        .pipe( gulp.dest( PLUGIN_DIST_DIR ) );               // save files into dist directory
 });
 
 // run image-related tasks
 gulp.task( 'images', function() {
     return gulp.src( UPLOADS_DIST_DIR + '**/**' )           // grab all dist image files
-        .pipe( plugins.changedInPlace() )                   // check if source has changed since last build
+        //.pipe( plugins.changedInPlace() )                   // check if source has changed since last build
         .pipe( plugins.webp() )                             // create WEBP versions of images
         .pipe( plugins.imagemin(                            // optimize & cache images in the uploads directory
             {
@@ -70,8 +66,7 @@ gulp.task( 'images', function() {
                 interlaced: true
             }
         ))
-        .pipe( gulp.dest( UPLOADS_DIST_DIR ) )              // save files into dist directory
-        .pipe( plugins.notify( 'Completed images' ) );      // send completion notification
+        .pipe( gulp.dest( UPLOADS_DIST_DIR ) );               // save files into dist directory
 });
 
 // let's get this party started!
