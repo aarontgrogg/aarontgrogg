d29d3db10c9abdad71bb42c77d7fe48a6f19ced4
Aaron T. Grogg <aarontgrogg@gmail.com>
2015-12-23 14:06:02 +0100

    testing build

--- gulpfile.js
+++ gulpfile.js
@@ -20,6 +20,7 @@ var gulp = require('gulp'),
 // run theme CSS-related tasks
 gulp.task( 'styles-theme', function() {
     return gulp.src( 'src/styles/*.css' )                   // grab all src css files
+        .pipe( plugins.changed( THEME_DIST_DIR ) )          // check if source has changed since last build
         .pipe( plugins.autoprefixer( AUTOPREFIXER_MATRIX ) ) // remove unneeded and add needed prefixes
         .pipe( plugins.concat( 'style-min.css' ) )          // concatenate all css files into a single files
         .pipe( plugins.minifyCss() )                        // minify concatenated css files
@@ -29,6 +30,7 @@ gulp.task( 'styles-theme', function() {
 // run theme JS-related tasks
 gulp.task( 'scripts-theme', function() {
     return gulp.src( 'src/scripts/*.js' )                   // grab all src js files
+        .pipe( plugins.changed( THEME_DIST_DIR ) )          // check if source has changed since last build
         .pipe( plugins.concat( 'scripts-min.js' ) )         // concatenate all js files into a single files
         .pipe( plugins.uglify() )                           // minify concatenated js files
         .pipe( gulp.dest( THEME_DIST_DIR ) );               // save files into dist directory
@@ -37,6 +39,7 @@ gulp.task( 'scripts-theme', function() {
 // run plugins CSS-related tasks
 gulp.task( 'styles-plugins', function() {
     return gulp.src( PLUGIN_DIST_DIR + '**/*.css' )         // grab all dist css files
+        .pipe( plugins.changedInPlace() )                   // check if source has changed since last build
         .pipe( plugins.autoprefixer( AUTOPREFIXER_MATRIX ) ) // remove unneeded and add needed prefixes
         .pipe( gulp.dest( PLUGIN_DIST_DIR) )                // save files into dist directory
         .pipe( plugins.minifyCss() )                        // minify concatenated css files
@@ -46,6 +49,7 @@ gulp.task( 'styles-plugins', function() {
 // run plugins JS-related tasks
 gulp.task( 'scripts-plugins', function() {
     return gulp.src( PLUGIN_DIST_DIR + '**/*.js' )          // grab all src js files
+        .pipe( plugins.changedInPlace() )                   // check if source has changed since last build
         .pipe( plugins.uglify() )                           // minify concatenated js files
         .pipe( gulp.dest( PLUGIN_DIST_DIR ) );               // save files into dist directory
 });
