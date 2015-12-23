d011350af83e7f63b21b0aa8086715438c8e098d
Aaron T. Grogg <aarontgrogg@gmail.com>
2015-12-23 13:14:19 +0100

    adding gulp-notification

--- gulpfile.js
+++ gulpfile.js
@@ -24,7 +24,8 @@ gulp.task( 'styles-theme', function() {
         .pipe( plugins.autoprefixer( AUTOPREFIXER_MATRIX ) ) // remove unneeded and add needed prefixes
         .pipe( plugins.concat( 'style-min.css' ) )          // concatenate all css files into a single files
         .pipe( plugins.minifyCss() )                        // minify concatenated css files
-        .pipe( gulp.dest( THEME_DIST_DIR ) );               // save files into dist directory
+        .pipe( gulp.dest( THEME_DIST_DIR ) )                // save files into dist directory
+        .pipe( plugins.notify( 'Completed styles-theme' ) ); // send completion notification
 });
 
 // run theme JS-related tasks
@@ -33,7 +34,8 @@ gulp.task( 'scripts-theme', function() {
         .pipe( plugins.changedInPlace() )                   // check if source has changed since last build
         .pipe( plugins.concat( 'scripts-min.js' ) )         // concatenate all js files into a single files
         .pipe( plugins.uglify() )                           // minify concatenated js files
-        .pipe( gulp.dest( THEME_DIST_DIR ) );               // save files into dist directory
+        .pipe( gulp.dest( THEME_DIST_DIR ) )                // save files into dist directory
+        .pipe( plugins.notify( 'Completed scripts-theme' ) ); // send completion notification
 });
 
 // run plugins CSS-related tasks
@@ -43,7 +45,8 @@ gulp.task( 'styles-plugins', function() {
         .pipe( plugins.autoprefixer( AUTOPREFIXER_MATRIX ) ) // remove unneeded and add needed prefixes
         .pipe( gulp.dest( PLUGIN_DIST_DIR) )                // save files into dist directory
         .pipe( plugins.minifyCss() )                        // minify concatenated css files
-        .pipe( gulp.dest( PLUGIN_DIST_DIR) );               // save files into dist directory
+        .pipe( gulp.dest( PLUGIN_DIST_DIR) )                // save files into dist directory
+        .pipe( plugins.notify( 'Completed styles-plugins' ) ); // send completion notification
 });
 
 // run plugins JS-related tasks
@@ -51,7 +54,8 @@ gulp.task( 'scripts-plugins', function() {
     return gulp.src( PLUGIN_DIST_DIR + '**/*.js' )          // grab all src js files
         .pipe( plugins.changedInPlace() )                   // check if source has changed since last build
         .pipe( plugins.uglify() )                           // minify concatenated js files
-        .pipe( gulp.dest( PLUGIN_DIST_DIR ) );              // save files into dist directory;
+        .pipe( gulp.dest( PLUGIN_DIST_DIR ) )               // save files into dist directory
+        .pipe( plugins.notify( 'Completed scripts-plugins' ) ); // send completion notification
 });
 
 // run image-related tasks
@@ -66,7 +70,8 @@ gulp.task( 'images', function() {
                 interlaced: true
             }
         ))
-        .pipe( gulp.dest( UPLOADS_DIST_DIR ) );             // save files into dist directory
+        .pipe( gulp.dest( UPLOADS_DIST_DIR ) )              // save files into dist directory
+        .pipe( plugins.notify( 'Completed images' ) );      // send completion notification
 });
 
 // let's get this party started!
