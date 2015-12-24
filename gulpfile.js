45977ab02700f2699c329a0de9132f34ef06c431
Aaron Grogg <a.grogg@netbiscuits.com>
2015-12-24 14:43:33 +0100

    polishing svg process

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 // define requirements
 var gulp = require('gulp'),
+    path = require('path'),
     plugins = require('gulp-load-plugins')(),
     AUTOPREFIXER_MATRIX = 'last 2 version',
     THEME_DIST_DIR = 'dist/wp-content/themes/atg',
@@ -7,9 +8,24 @@ var gulp = require('gulp'),
 
 // run SVG-related tasks
 /* 
+    https://css-tricks.com/svg-symbol-good-choice-icons/
+    - don't pay too much attention, just get the idea, then move on to...
     https://css-tricks.com/svg-use-with-external-reference-take-2/
     - can have icons in an external sprite, ref via use, benefit from browser caching, but harder to style
-    - maybe doesn't matter for me, since icons are all solid color, make sure :hover can work
+        - maybe doesn't matter for me, since icons are all solid color, make sure :hover can work
+    - more importantly, doesn't work in any IE, and only coming to Edge soon
+    - so probably @include into any page, using functions.php?
+        - include needs to go at top of document
+        - be sure included <svg> gets display: none
+        - then each icon, inside the <a>, can be:
+            <svg class="icon"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#github"></use></svg>
+        - and CSS will cascade
+            * need adjustments to current site CSS
+    - had to remove <rect> from initial downloaded icons, so :hover would work properly
+    - removed "-" from each file name & id so minify/concat would work properly (prob need an automated way to do this?)
+    - added <title> and <desc> to each SVG
+
+    - use of @include and <use> seems like it will work for me and my tiny icon set; YMMV
 
     https://www.liquidlight.co.uk/blog/article/creating-svg-sprites-using-gulp-and-sass/
 
@@ -20,11 +36,32 @@ var gulp = require('gulp'),
     - gulp-svg-sprite (optimizes, creates sprite and CSS; https://www.npmjs.com/package/gulp-svg-sprite)
     needed?
     - gulp-svg2png (creates PNG fallbacks; https://www.npmjs.com/package/gulp-svg2png)
+
+    * choice:
+        - tried gulp-svg-sprite, because one seems better than two, but config was too complicated-looking
+        - also, liked the idea of using svgo because, who argues with Jake?
+        - slight hang-up on use of Node.js' "path", easy to think *everything* must be gulp...
+        - still deciding on if need svg fallbacks
+            - https://github.com/jonathantneal/svg4everybody
+            - https://github.com/w0rm/gulp-svgfallback
 */
-/*gulp.task('icons', function() {
+gulp.task('icons', function() {
     return gulp.src('src/icons/*.svg')                      // grab all src svg files
-        .pipe(...); // save files into dist directory
-});*/
+        .pipe(plugins.svgmin(function (file) {              // minify all svg files
+            var prefix = path.basename(file.relative, path.extname(file.relative)); // create unique prefix for each symbol's ID
+            return {
+                plugins: [{
+                    cleanupIDs: {
+                        minify: true
+                    }
+                }]
+            }
+        }))
+        .pipe(plugins.svgstore({                            // concatenate all icon svgs into a single file
+            inlineSvg: true                                 // exclude 
+        }))
+        .pipe(gulp.dest( THEME_DIST_DIR + '/icons'))        // save files into dist directory
+});
 
 // run theme CSS-related tasks
 gulp.task( 'styles-theme', function() {
@@ -64,4 +101,4 @@ gulp.task( 'scripts-plugins', function() {
 });
 
 // let's get this party started!
-gulp.task('default', [ /*'icons', */'styles-theme', 'scripts-theme', 'styles-plugins', 'scripts-plugins' ]);
+gulp.task('default', [ 'icons', 'styles-theme', 'scripts-theme', 'styles-plugins', 'scripts-plugins' ]);
