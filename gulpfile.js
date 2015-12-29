7f34f96a1fee60b07bbfd8a710527e12a88f0401
Aaron T. Grogg <aarontgrogg@gmail.com>
2015-12-29 10:33:43 +0100

    finishing svg, removing cruft

--- gulpfile.js
+++ gulpfile.js
@@ -7,44 +7,6 @@ var gulp = require('gulp'),
     PLUGIN_DIST_DIR = 'dist/wp-content/plugins/';
 
 // run SVG-related tasks
-/* 
-    https://css-tricks.com/svg-symbol-good-choice-icons/
-    - don't pay too much attention, just get the idea, then move on to...
-    https://css-tricks.com/svg-use-with-external-reference-take-2/
-    - can have icons in an external sprite, ref via use, benefit from browser caching, but harder to style
-        - maybe doesn't matter for me, since icons are all solid color, make sure :hover can work
-    - more importantly, doesn't work in any IE, and only coming to Edge soon
-    - so probably @include into any page, using functions.php?
-        - include needs to go at top of document
-        - be sure included <svg> gets display: none
-        - then each icon, inside the <a>, can be:
-            <svg class="icon"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#github"></use></svg>
-        - and CSS will cascade
-            * need adjustments to current site CSS
-    - had to remove <rect> from initial downloaded icons, so :hover would work properly
-    - removed "-" from each file name & id so minify/concat would work properly (prob need an automated way to do this?)
-    - added <title> and <desc> to each SVG for a11y
-
-    - use of @include and <use> seems like it will work for me and my tiny icon set; YMMV
-
-    https://www.liquidlight.co.uk/blog/article/creating-svg-sprites-using-gulp-and-sass/
-
-    check diffs:
-    - gulp-svgmin (minifies SVGs using SVGO; https://github.com/ben-eb/gulp-svgmin)
-    - gulp-svgstore (creates sprite; https://www.npmjs.com/package/gulp-svgstore)
-    versus:
-    - gulp-svg-sprite (optimizes, creates sprite and CSS; https://www.npmjs.com/package/gulp-svg-sprite)
-    needed?
-    - gulp-svg2png (creates PNG fallbacks; https://www.npmjs.com/package/gulp-svg2png)
-
-    * choice:
-        - tried gulp-svg-sprite, because one seems better than two, but config was too complicated-looking
-        - also, liked the idea of using svgo because, who argues with Jake?
-        - slight hang-up on use of Node.js' "path", easy to think *everything* must be gulp...
-        - still deciding on if need svg fallbacks
-            - https://github.com/jonathantneal/svg4everybody
-            - https://github.com/w0rm/gulp-svgfallback
-*/
 gulp.task('icons', function() {
     return gulp.src('src/icons/*.svg')                      // grab all src svg files
         .pipe(plugins.svgmin(function (file) {              // minify all svg files
