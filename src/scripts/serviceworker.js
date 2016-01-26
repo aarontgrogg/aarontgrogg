ea292d7a2d1ba7a7d960532cda2adcea325e532a
Aaron T. Grogg <aarontgrogg@gmail.com>
2016-01-26 12:47:12 +0100

    tried adding cache-buster...

--- src/scripts/serviceworker.js
+++ src/scripts/serviceworker.js
@@ -47,7 +47,7 @@ self.addEventListener('fetch', function(event) {
             // to clone it so we have 2 stream.
             var responseToCache = response.clone();
 
-            caches.open(CACHE_NAME)
+            caches.open(cachename)
               .then(function(cache) {
                 cache.put(event.request, responseToCache);
               });
